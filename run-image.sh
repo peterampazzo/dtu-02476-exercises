061250dfb4ef939eb3ddc6349e75b49d7c57c222
Pietro Rampazzo <pietro@rampazzo.co>
2022-01-12 10:04:55 +0100

    cleanup

--- run-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run \
-  --name experiment2 \
-  --mount type=bind,src="$(pwd)"/models,dst=/models/ \
-  --mount type=bind,src="$(pwd)"/data,dst=/data/ \
-  --mount type=bind,src="$(pwd)"/reports,dst=/reports/ \
-  -d trainer:latest 
-
